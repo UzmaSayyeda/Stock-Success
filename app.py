849b4e79df82e452453bc5a2b688c14365a18b95
UzmaSayyeda <uzmafatima5991@gmail.com>
2023-10-12 20:39:43 -0500

    fixed template error

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("templates\index.html")
+    return render_template("index.html")
 
 @app.route("/api/data")
 def get_data():
