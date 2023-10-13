cfa069b5d34856b27abfe872654b289e17248868
UzmaSayyeda <uzmafatima5991@gmail.com>
2023-10-12 20:16:52 -0500

    created folders and app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("templates\index.html")
+
+@app.route("/api/data")
+def get_data():
+    conn = sqlite3.connect("test_stock_market.db")
+    cursor = conn.cursor()
+    query = "SELECT * FROM test_stock_market"
+    data = cursor.execute(query).fetchall()
+    conn.close()
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
